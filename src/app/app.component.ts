8cf27708302e2bdfd82d86c16326fdd79e2b948b
COSSU_A <amandamcossu@gmail.com>
2019-12-02 22:10:15 +0100

    hello Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+/*
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pokemon-app';
-}
+}*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pokemon-app',
+  template: `<h1>Hello {{name}}</h1>`,
+})
+export class AppComponent  { name = 'Angular'; }
