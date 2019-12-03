bc985704f528a1d85e2bbb26d4755e59a232cc18
COSSU_A <amandamcossu@gmail.com>
2019-12-03 15:04:09 +0100

    personalized pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { BorderCardDirective } from './border-card.directive';
+import { PokemonTypeColorPipe} from './pokemon-type-color.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BorderCardDirective
+    BorderCardDirective,
+    PokemonTypeColorPipe
   ],
   imports: [
     BrowserModule
