1a03e94983c4265d10de2b06906c4b43b0b67e3c
COSSU_A <amandamcossu@gmail.com>
2020-01-07 16:02:18 +0100

    update project 2020

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,13 @@ import { AppComponent } from './app.component';
 
 import { BorderCardDirective } from './border-card.directive';
 import { PokemonTypeColorPipe} from './pokemon-type-color.pipe';
-import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
-import { WeatherComponent } from './weather/weather.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     BorderCardDirective,
     PokemonTypeColorPipe,
-    DetailPokemonComponent,
-    WeatherComponent
   ],
   imports: [
     BrowserModule
